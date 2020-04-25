90bdac4cd7766d8f3e90139ee96c68cb1e9f53da
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-04-25 13:18:27 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/bookingdemo/BookingDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bookingdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookingDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bookingdemo/BookingDemoApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.bookingdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
