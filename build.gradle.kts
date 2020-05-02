64a81a0ca4ba2a2b39de3171d96d7a64b86f6e24
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-05-02 15:28:49 +0200

    Populate rooms on startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.mongobee:mongobee:0.13")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
---
+++ src/main/kotlin/com/example/bookingdemo/persistence/DataMigrations.kt
@@ -0,0 +1,38 @@
+package com.example.bookingdemo.persistence
+
+import com.example.bookingdemo.model.Room
+import com.github.mongobee.changeset.ChangeLog
+import com.github.mongobee.changeset.ChangeSet
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@ChangeLog(order = "001")
+class DataMigrations {
+    @ChangeSet(order = "001", author = "lukasz.pieta", id = "rooms created")
+    fun createRooms(mongoTemplate: MongoTemplate) {
+        val room1A = Room(
+            number = "1A",
+            hasProjector = false,
+            hasWhiteboard = true
+        )
+        val room1B = Room(
+            number = "1B",
+            hasProjector = true,
+            hasWhiteboard = false
+        )
+        val room2A = Room(
+            number = "2A",
+            hasProjector = false,
+            hasWhiteboard = false
+        )
+        val room2B = Room(
+            number = "2B",
+            hasProjector = true,
+            hasWhiteboard = true
+        )
+        mongoTemplate.insert(room1A)
+        mongoTemplate.insert(room1B)
+        mongoTemplate.insert(room2A)
+        mongoTemplate.insert(room2B)
+        println("Applied changeset 001 - rooms created")
+    }
+}
