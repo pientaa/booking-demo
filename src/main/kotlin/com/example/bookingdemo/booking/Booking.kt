8c08e530c30f195b52d6b3fbcc3f79ef8dbef618
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-05-02 14:15:54 +0200

    Change String id to UUID

--- src/main/kotlin/com/example/bookingdemo/booking/Booking.kt
+++ src/main/kotlin/com/example/bookingdemo/booking/Booking.kt
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
+import java.util.*
 
 @Document
 data class Booking(
     @Id
-    val id: String,
+    val id: UUID = UUID.randomUUID(),
     val roomId: String,
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     val start: Instant,
--- src/main/kotlin/com/example/bookingdemo/room/Room.kt
+++ src/main/kotlin/com/example/bookingdemo/room/Room.kt
@@ -2,11 +2,12 @@ package com.example.bookingdemo.room
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
 
 @Document
 data class Room(
     @Id
-    val id: String,
+    val id: UUID = UUID.randomUUID(),
     val number: String,
     val hasWhiteboard: Boolean,
     val hasProjector: Boolean
