54de3e550dd1dbf23fb6cbfa74f793ef5b234b57
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-05-13 18:05:44 +0200

    Delete unnecessary imports

--- src/main/kotlin/com/example/bookingdemo/model/Room.kt
+++ src/main/kotlin/com/example/bookingdemo/model/Room.kt
@@ -3,7 +3,6 @@ package com.example.bookingdemo.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import java.util.*
 
 @Document
 data class Room(
